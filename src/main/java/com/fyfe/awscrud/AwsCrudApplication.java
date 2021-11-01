ae539131bc4c5666e8b26c307a9d0bf442b235bf
grahamfyfecodes <67826423+grahamfyfecodes@users.noreply.github.com>
2021-11-01 14:05:57 +0000

    Init Commit

---
+++ src/main/java/com/fyfe/awscrud/AwsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.fyfe.awscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyfe/awscrud/AwsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyfe.awscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
